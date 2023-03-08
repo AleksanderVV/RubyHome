5a004271b5aa85649da9cdd95e0c35e27e36888a
Aleksander <voronyuk.aleksander@gmail.com>
2023-03-08 14:57:01 +0200

    continue work javascript in header tabs

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -6,38 +6,50 @@ import { flsModules } from "./modules.js";
 import bootstrap from 'bootstrap';
 
 
-function selectOpen(label,select) {
+// function selectOpen(label,select) {
+//   const labelUse = document.querySelector(label);
+//   const selectUse = document.getElementById(select);
+
+//   labelUse.addEventListener('click', () => {
+
+//     let event = new MouseEvent('mousedown', {
+//       'view': window,
+//       'bubbles': true,
+//       'cancelable': true
+//     });
+//     selectUse.dispatchEvent(event);
+
+//     event = new MouseEvent('mouseup', {
+//       'view': window,
+//       'bubbles': true,
+//       'cancelable': true
+//     });
+//     selectUse.dispatchEvent(event);
+//   });
+// }
+function selectOpen(label, select) {
   const labelUse = document.querySelector(label);
   const selectUse = document.getElementById(select);
 
   labelUse.addEventListener('click', () => {
-
-    let event = new MouseEvent('mousedown', {
-      'view': window,
-      'bubbles': true,
-      'cancelable': true
-    });
-    selectUse.dispatchEvent(event);
-
-    event = new MouseEvent('mouseup', {
-      'view': window,
-      'bubbles': true,
-      'cancelable': true
-    });
-    selectUse.dispatchEvent(event);
+    selectUse.focus();
   });
 }
 
-selectOpen('label[for="buy-location-select"]','buy-location-select');
-selectOpen('label[for="buy-type-select"]','buy-type-select');
-selectOpen('label[for="buy-price-select"]','buy-price-select');
+selectOpen('label[for="buy-location-select"]', 'buy-location-select');
+selectOpen('label[for="buy-type-select"]', 'buy-type-select');
+selectOpen('label[for="buy-price-select"]', 'buy-price-select');
 
-selectOpen('label[for="rent-location-select"]','rent-location-select');
-selectOpen('label[for="rent-type-select"]','rent-type-select');
-selectOpen('label[for="rent-price-select"]','rent-price-select');
+// selectOpen('label[for="buy-location-select"]','buy-location-select');
+// selectOpen('label[for="buy-type-select"]','buy-type-select');
+// selectOpen('label[for="buy-price-select"]','buy-price-select');
 
-selectOpen('label[for="sell-location-select"]','sell-location-select');
-selectOpen('label[for="sell-type-select"]','sell-type-select');
-selectOpen('label[for="sell-price-select"]','sell-price-select');
+// selectOpen('label[for="rent-location-select"]','rent-location-select');
+// selectOpen('label[for="rent-type-select"]','rent-type-select');
+// selectOpen('label[for="rent-price-select"]','rent-price-select');
+
+// selectOpen('label[for="sell-location-select"]','sell-location-select');
+// selectOpen('label[for="sell-type-select"]','sell-type-select');
+// selectOpen('label[for="sell-price-select"]','sell-price-select');
 
 
